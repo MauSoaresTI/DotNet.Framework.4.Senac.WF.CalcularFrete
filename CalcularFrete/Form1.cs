5ad6ef335875467192eeafb74b1011e64f5b1d8f
Mauricio <mauprincipal@hotmail.com>
2023-10-02 20:56:23 -0300

    Alterei a altura do form

--- CalcularFrete/Form1.cs
+++ CalcularFrete/Form1.cs
@@ -16,5 +16,39 @@ namespace CalcularFrete
         {
             InitializeComponent();
         }
+
+        private void btnCalcularFrete_Click(object sender, EventArgs e)
+        {
+            var freteMinimo = Convert.ToDecimal( txtFreteMinimo.Text );
+            var uf = txtUf.Text;
+
+            CalcularFrete(freteMinimo, uf);
+        }
+
+
+       void CalcularFrete(decimal freteMinimo, String uf)
+        {
+            //VARIAVEL DO TIPO DECIMAL
+
+            var adicional = 5.0M;
+            var freteFinal = freteMinimo;
+
+            //SE UF FOR IGUAL == A RIO GRANDE DO SUL, VAI ENTRAR NO IF
+
+            if (uf.Equals("RS")) 
+            {
+            
+                freteFinal = freteMinimo + 0.5M;
+            //Equals é USADO PARA DEIXAR IGUAL COMO == que vai ser o Equals
+            }else if (uf.Equals("SC"))
+            {
+                freteFinal = freteMinimo + 1.0M;
+            }
+            else 
+            {
+                freteFinal = freteMinimo + adicional;
+            }
+            txtTotalFrete.Text = freteFinal.ToString("F2");
+        }
     }
 }
